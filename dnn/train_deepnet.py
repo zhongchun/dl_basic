c6fdfb8995b3d15732fdbcc2a97ef7b460a0be6b
zhongchun <zhongchunyu@gmail.com>
2019-01-22 07:45:38 +0800

    add some dnn scripts

---
+++ dnn/train_deepnet.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+ @FileName: train_deepnet
+ @Desc:  
+ @Author: yuzhongchun
+ @Date: 2019-01-22 07:21
+ @Last Modified by: yuzhongchun
+ @Last Modified time: 2019-01-22 07:21
+"""
+
+import sys, os
+
+sys.path.append(os.pardir)
+import numpy as np
+import matplotlib.pyplot as plt
+from dataset.mnist import load_mnist
+from deep_convnet import DeepConvNet
+from common.trainer import Trainer
+
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=False)
+
+network = DeepConvNet()
+trainer = Trainer(network, x_train, t_train, x_test, t_test,
+                  epochs=20, mini_batch_size=100,
+                  optimizer='Adam', optimizer_param={'lr': 0.001},
+                  evaluate_sample_num_per_epoch=1000)
+trainer.train()
+
+network.save_params("deep_convnet_params.pkl")
+print("Saved Network Parameters!")
